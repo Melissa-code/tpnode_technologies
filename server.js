bda3435400da9563983aae12be2ad9fe4a7303fe
Melissa Benard <benardmeli@yahoo.fr>
2024-01-02 21:55:01 +0100

    Get all the messages written by a user

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ app.post('/commentaire', async function (req, res) {
 }); 
 
 /**
- * Get the first letter in upper case
+ * Capitalize the first letter 
  * @param {*} str 
  * @returns 
  */
@@ -155,7 +155,7 @@ function strUcFirst(str) {
 }
 
 /**
- * Get all the feedback about a technology in the database
+ * Get all the feedbacks about a technology in the database
  */
 app.get('/commentaires/:nomTechnologie', async function (req, res) {
     try {
@@ -176,6 +176,24 @@ app.get('/commentaires/:nomTechnologie', async function (req, res) {
     } catch (err) {
         console.error('Erreur lors de la récupération des commentaires sur une technologie :', err);
         res.status(500).json({ error: 'Une erreur est survenue lors de la récupération des commentaires sur une technologie.' });
+    } 
+});
+
+/**
+ * Get all the messages written by a user in the database
+ */
+app.get('/messages/:userId', async function (req, res) {
+    try {
+        let userId = parseInt(req.params.userId);
+        if (userId <= 0) {
+            return res.status(400).json({ error: "L'identifiant d'utilisateur doit être un nombre entier positif." });
+        }
+       
+        const [result, field] = await db.query('SELECT c.id, c.date_creation_commentaire, u.nom, u.prenom FROM commentaire AS c INNER JOIN utilisateur AS u ON c.utilisateur_id = u.id WHERE c.utilisateur_id = ?', [userId]);
+        res.status(200).json(result);
+    } catch (err) {
+        console.error('Erreur lors de la récupération des commentaires rédigés par une personne :', err);
+        res.status(500).json({ error: 'Une erreur est survenue lors de la récupération des commentaires rédigés par une personne.' });
     } 
 });
 
